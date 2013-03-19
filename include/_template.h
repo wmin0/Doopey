d1046dd1905f0216c46c83adea9e0396d75071e1
wmin0 <wmin0@hotmail.com>
2013-03-20 00:29:56 +0800

    block adt

---
+++ include/_template.h
@@ -0,0 +1,9 @@
+#ifndef _DOOPEY_H_
+#define _DOOPEY_H_
+
+namespace Doopey {
+  
+
+};  // namespace Doopey
+
+#endif
---
+++ include/block/DataDelegate.h
@@ -0,0 +1,9 @@
+#ifndef _DOOPEY_DATADELEGATE_H_
+#define _DOOPEY_DATADELEGATE_H_
+
+namespace Doopey {
+  
+
+};  // namespace Doopey
+
+#endif
