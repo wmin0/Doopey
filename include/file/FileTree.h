f223f82a088689f6d35021be449d74fdbe8d73cb
Jian-Wei Wu <i.lu.f@hotmail.com.tw>
2013-04-17 16:05:14 +0800

    Add HashList class to implement hash table

--- include/file/FileTree.h
+++ include/file/FileTree.h
@@ -3,10 +3,35 @@
 
 //#include <ext/hash_map>
 #include <string>
+#include <functional>
 
 using std::string;
+using std::hash;
 
-namespace Dooepy{
+namespace Doopey{
+
+  template<typename KEY, typename VALUE>
+  class HashList
+  {
+    struct HashItem
+    {
+       KEY key;
+       VALUE value;
+       HashItem* next;
+    };
+
+    public:
+      HashList(uint64_t size = 3000);
+      bool add(const KEY& key, const VALUE& value);
+      bool remove(const KEY& key);
+      VALUE getValue(const KEY& key);
+    
+    private:
+      hash<KEY> keyHash;
+
+      HashItem** _list;
+      const uint64_t _listSize;
+  };//Class HashList
 
   class TreeNode{
     friend class FileTree;
@@ -25,8 +50,9 @@ namespace Dooepy{
       FileTree();
       ~FileTree();
     private:
-//      hash_map<string, void*> _fileMap;
+      HashList<string, TreeNode*> _fileTable;
   };//class FileTree
+
 
 }//namespace Doopey
 
--- src/file/FileTree.cpp
+++ src/file/FileTree.cpp
@@ -1,11 +1,80 @@
+#include <functional>
+#include <string>
+
 #include "file/FileTree.h"
 
-//FileTree::FileTree()
-//{
-//}
+using namespace Doopey;
+using std::string;
 
-//FileTree::~FileTree()
-//{
-//}
+template<typename KEY, typename VALUE>
+HashList<KEY,VALUE>::HashList(uint64_t size):_listSize(size)
+{
+  _list = new HashItem*[size];
+  for(uint64_t i=0; i<size; i++)
+    _list[i] = NULL;
+}
 
+template<typename KEY, typename VALUE>
+bool HashList<KEY,VALUE>::add(const KEY& key, const VALUE& value)
+{
+  uint64_t index = keyHash(key)%_listSize;
+  if(_list[index] == NULL)//means it is the first element
+  {
+    _list[index] = new HashItem(key, value, NULL);  
+  }else{//means it is not the first
+    HashItem* saveplace = _list[index];
+    while(saveplace->next != NULL)
+    {
+      saveplace = saveplace->next;
+    }
+    saveplace->next = new HashItem(key, value, NULL);
+  }
+  return true;
+}
+
+template<typename KEY, typename VALUE>
+bool HashList<KEY,VALUE>::remove(const KEY& key)
+{
+  uint64_t index = keyHash(key)%_listSize;
+  HashItem* pos = _list[index];
+  if(_list[index]->key == key)
+  {
+    _list[index] = _list[index]->next;
+    free(pos);
+    return true;
+  }
+  pos = _list[index]->next;
+  HashItem* ppos = _list[index];
+  while(pos!=NULL)
+  {
+    if(pos->key == key)
+    {
+      ppos->next = pos->next;
+      free(pos);
+      return true;
+    }
+  }
+  return false;
+}
+
+template<typename KEY, typename VALUE>
+VALUE HashList<KEY, VALUE>::getValue(const KEY& key)
+{
+  uint64_t index = keyHash(key)%_listSize;
+  HashItem* pos = _list[index];
+  while(pos!=NULL)
+  {
+    if(pos->key == key)
+      return pos->value;
+  }
+  return NULL;
+}
+
+FileTree::FileTree()
+{
+}
+
+FileTree::~FileTree()
+{
+}
 
