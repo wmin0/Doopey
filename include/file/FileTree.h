2d32ee79add2a35b860318fbb74b8f53bebc2913
wmin0 <wmin0@hotmail.com>
2013-04-20 11:15:03 +0800

    another way to fix template dependency

--- include/file/FileTree.h
+++ include/file/FileTree.h
@@ -5,6 +5,7 @@
 #include <functional>
 #include <common/Doopey.h>
 
+
 using std::string;
 using std::hash;
 
@@ -21,90 +22,18 @@ namespace Doopey{
     };
 
     public:
-      HashList(uint64_t size = 3000): _listSize(size)
-      {
-        _list = new HashItem*[size];
-        for(uint64_t i=0; i<size; i++)
-          _list[i] = NULL;
-      }
-      ~HashList()
-      {
-        for(uint64_t i=0; i<_listSize; i++)
-        {
-          HashItem* release = _list[i];
-          HashItem* next;
-          while(release!=NULL)
-          {
-            next = release->next;
-            free(release);
-            release = next;
-          }
-        }
-        free(_list);
-      }
-      bool add(const KEY& key, const VALUE& value)
-      {
-        uint64_t index = keyHash(key)%_listSize;
-        if(_list[index] == NULL)//means it is the first element
-        {
-          _list[index] = new HashItem();
-          _list[index]->key = key;
-          _list[index]->value = value;
-          _list[index]->next = NULL;
-        }else{//means it is not the first
-          HashItem* saveplace = _list[index];
-          while(saveplace->next != NULL)
-          {
-            saveplace = saveplace->next;
-          }
-          saveplace->next = new HashItem();
-          saveplace = saveplace->next;
-          saveplace->key = key;
-          saveplace->value = value;
-          saveplace->next = NULL;
-        }
-        return true;
-      }
-      bool remove(const KEY& key)
-      {
-        uint64_t index = keyHash(key)%_listSize;
-        HashItem* pos = _list[index];
-        if(_list[index]->key == key)
-        {
-          _list[index] = _list[index]->next;
-          free(pos);
-          return true;
-        }
-        pos = _list[index]->next;
-        HashItem* ppos = _list[index];
-        while(pos!=NULL)
-        {
-          if(pos->key == key)
-          {
-            ppos->next = pos->next;
-            free(pos);
-            return true;
-          }
-        }
-        return false;
-      }
-      VALUE getValue(const KEY& key)
-      {
-        uint64_t index = keyHash(key)%_listSize;
-        HashItem* pos = _list[index];
-        while(pos!=NULL)
-        {
-          if(pos->key == key)
-            return pos->value;
-        }
-        return NULL;
-      }
+      HashList(uint64_t size = 3000);
+      ~HashList();
+      bool add(const KEY& key, const VALUE& value);
+      bool remove(const KEY& key);
+      VALUE getValue(const KEY& key);
     private:
       hash<KEY> keyHash;
 
       HashItem** _list;
       const uint64_t _listSize;
   };//Class HashList
+  #include "HashList.tpp"
 
   class TreeNode{
     public:
